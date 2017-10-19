1c1f9da865039a240ae81917aec27067a4e95534
Leporoni <leporoni@hotmail.com>
2017-10-19 13:44:42 -0200

    Signed-off-by: Leporoni <leporoni@hotmail.com>

---
+++ guest-party-management/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into convidado (id, nome, quantidade_acompanhantes) values (1, 'Aquaman', 2);
+insert into convidado (id, nome, quantidade_acompanhantes) values (2, 'Wonder Woman', 3);
+insert into convidado (id, nome, quantidade_acompanhantes) values (3, 'Batman', 1);
+insert into convidado (id, nome, quantidade_acompanhantes) values (4, 'Flash', 3);
+insert into convidado (id, nome, quantidade_acompanhantes) values (5, 'Cyborg', 4);
+insert into convidado (id, nome, quantidade_acompanhantes) values (6, 'Superman', 1);
+insert into convidado (id, nome, quantidade_acompanhantes) values (7, 'Jocker', 5);
+insert into convidado (id, nome, quantidade_acompanhantes) values (8, 'Lex Luthor', 3);
+insert into convidado (id, nome, quantidade_acompanhantes) values (9, 'Penguin', 10);
